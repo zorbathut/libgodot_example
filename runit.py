a13ee17f62a755d89518f231c774b44e174c99db
Ben Rog-Wilhelm <zorba-git-sharon-vfio@pavlovian.net>
2025-09-10 05:53:35 -0500

    Windows runit compat.

--- runit.py
+++ runit.py
@@ -3,6 +3,21 @@
 import subprocess
 import os
 import sys
+import platform
+import shutil
+
+# Platform-specific settings
+is_windows = platform.system() == "Windows"
+if is_windows:
+    godot_exe = "godot/bin/godot.windows.editor.dev.x86_64.executable.exe"
+    driver_exe = "driver/driver.exe"
+    lib_path_var = "PATH"
+    path_separator = ";"
+else:
+    godot_exe = "./bin/godot.linuxbsd.editor.dev.x86_64.executable"
+    driver_exe = "./driver/driver"
+    lib_path_var = "LD_LIBRARY_PATH"
+    path_separator = ":"
 
 print("Initializing Git submodules...")
 subprocess.run(["git", "submodule", "init"], check=True)
@@ -12,11 +27,11 @@ print("Running scons in the godot directory...")
 subprocess.run(["scons", "extra_suffix=executable", "dev_build=yes", "debug_symbols=yes"], cwd="godot", check=True)
 
 print("Generating extension API and interface files...")
-subprocess.run(["./bin/godot.linuxbsd.editor.dev.x86_64.executable", "--dump-extension-api", "--dump-gdextension-interface", "--headless"], cwd="godot", check=True)
+subprocess.run([godot_exe, "--dump-extension-api", "--dump-gdextension-interface", "--headless"], cwd="godot", check=True)
 
 print("Moving generated files to godot-cpp/gdextension/...")
-subprocess.run(["mv", "godot/extension_api.json", "godot-cpp/gdextension/extension_api.json"], check=True)
-subprocess.run(["mv", "godot/gdextension_interface.h", "godot-cpp/gdextension/gdextension_interface.h"], check=True)
+shutil.move("godot/extension_api.json", "godot-cpp/gdextension/extension_api.json")
+shutil.move("godot/gdextension_interface.h", "godot-cpp/gdextension/gdextension_interface.h")
 
 print("Running scons in the godot-cpp directory...")
 subprocess.run(["scons", "debug_symbols=yes", "dev_build=yes", "optimize=debug"], cwd="godot-cpp", check=True)
@@ -34,7 +49,8 @@ if "--no-run" in sys.argv:
 
 print("Running driver...")
 env = os.environ.copy()
-env["LD_LIBRARY_PATH"] = os.path.abspath("godot/bin") + ":" + env.get("LD_LIBRARY_PATH", "")
-subprocess.run(["./driver/driver"], env=env, check=True)
+lib_path = os.path.abspath("godot/bin")
+env[lib_path_var] = lib_path + path_separator + env.get(lib_path_var, "")
+subprocess.run([driver_exe], env=env, check=True)
 
 print("Done!")
